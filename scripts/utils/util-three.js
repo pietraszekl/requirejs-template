f0965db6a7a51c841b0270ee656af57c051bbb09
Lukasz Pietraszek <pietraszek@hotmail.co.uk>
2016-10-22 11:41:45 +0100

    adding binding events

--- scripts/utils/util-three.js
+++ scripts/utils/util-three.js
@@ -1,3 +1,27 @@
-define(['jquery'], function($){
+define(['jquery'], function ($) {
+
   console.log('util three defined');
+
+  function clickHeaders() {
+    console.log('clickHeaders');
+    var header = 'h2';
+
+    $(header).on('click', function () {
+      console.log('clicked');
+      $(this).hide();
+    })
+  }
+
+  function bindEvents() {
+    console.log('bindEvents')
+    clickHeaders();
+  }
+
+  function init() {
+    bindEvents();
+  }
+
+  return {
+    load: init()
+  };
 })
